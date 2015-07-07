cb81669a5a22fcb6e10134477edc64139428eb5b
Wes Breisch <wabreisch@quinnipiac.edu>
2015-07-07 00:55:41 -0400

    - Please work 😁

--- personal.js
+++ personal.js
@@ -10,7 +10,7 @@ if (Meteor.isClient) {
 
   Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
+      // increment the counter when button is clicked please work
       Session.set('counter', Session.get('counter') + 1);
     }
   });
