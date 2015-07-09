757e89e95e267672479b06fef758289aef84d4ad
Wes Breisch <wabreisch@quinnipiac.edu>
2015-07-09 00:43:46 -0400

    - Adds more functionality for posting blog entries

--- client/templates/admin/admin.js
+++ client/templates/admin/admin.js
@@ -6,11 +6,37 @@ Template.admin.helpers({
 
 Template.admin.events({
   'click #blog': function () {
-    BlogPosts.insert({
-      author: Meteor.userId(),
-      created: new Date(),
-      title: "Test Title",
-      content: "Lorem ipsum dolor sit amet"
-    });
+    // BlogPosts.insert({
+    //   author: Meteor.userId(),
+    //   created: new Date(),
+    //   title: "Test Title",
+    //   content: "Lorem ipsum dolor sit amet"
+    // });
+    Router.go('newBlogPost');
   }
 });
+
+/* Template.postSubmit.events({
+  'submit form': function (e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
+    Meteor.call('postInsert', post, function(error, result) {
+      if (error)
+        Errors.throw(error.reason);
+
+      if ( result.postExists )
+        Errors.throw('This link has already been posted.');
+
+      Router.go('postPage', {_id: result._id});
+    });
+  }
+}); */
---
+++ client/templates/blog/newblogpost.js
@@ -0,0 +1,39 @@
+// Template.postSubmit.events({
+//   'submit form': function (e) {
+//     e.preventDefault();
+
+//     var post = {
+//       url: $(e.target).find('[name=url]').val(),
+//       title: $(e.target).find('[name=title]').val()
+//     };
+
+//     var errors = validatePost(post);
+//     if (errors.title || errors.url)
+//       return Session.set('postSubmitErrors', errors);
+
+//     Meteor.call('postInsert', post, function(error, result) {
+//       if (error)
+//         Errors.throw(error.reason);
+
+//       if ( result.postExists )
+//         Errors.throw('This link has already been posted.');
+
+//       Router.go('postPage', {_id: result._id});
+//     });
+//   }
+// });
+
+Template.newBlogPost.events({
+  'submit form': function (e) {
+    e.preventDefault();
+
+    var blogPost = {
+      title: $(e.target).find('[name=title]').val(),
+      content: $(e.target).find('[name=content]').val(),
+      author: Meteor.userId(),
+      created: new Date()
+    };
+
+    // Router.go('blog');
+  }
+});
